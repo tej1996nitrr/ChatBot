b069b558072cdd3b29f8310e74bf1e5f37aafc4a
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2019-12-08 15:20:55 +0530

    mapping lines and conversations

--- Tensorflow/chat.py
+++ Tensorflow/chat.py
@@ -11,3 +11,21 @@ conversations = open('Data/movie_conversations.txt',encoding='utf-8',errors='ign
 # %%
 '''making dictionary to map each line with id'''
 
+id2line={}
+for line in lines:
+    line_=line.split(' +++$+++ ')
+    if len(line_)==5:
+        id2line[line_[0]]=line_[4]
+    
+    
+
+# %%
+'''creating a list of all the conversations'''
+
+conversations_ids = []
+for conv in conversations[:-1]:
+    conv_ = conv.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","") #take last element
+#removed square brackets,'',and spaces
+    conversations_ids.append(conv_.split(','))     
+
+# %%
