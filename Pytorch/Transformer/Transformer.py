e7773caae78031fbf04f62919223289486257814
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-01-01 15:35:45 +0530

    added multihead attention

--- Pytorch/Transformer/Transformer.py
+++ Pytorch/Transformer/Transformer.py
@@ -197,5 +197,52 @@ class Embeddings(nn.Module):
         embedding = self.dropout(embedding)
         return embedding
 # %%
+class MultiHeadAttention(nn.Module):
+    
+    def __init__(self, heads, d_model):
+        
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % heads == 0
+        self.d_k = d_model // heads  # d_k dimetionality of each head D_MODEL = EMBEDDING_DIM
+        self.heads = heads
+        self.dropout = nn.Dropout(0.1)
+        self.query = nn.Linear(d_model, d_model)
+        self.key = nn.Linear(d_model, d_model)
+        self.value = nn.Linear(d_model, d_model)
+        self.concat = nn.Linear(d_model, d_model)
+    
+    def forward(self, query, key, value, mask):
+        """
+        query, key, value of shape: (batch_size, max_len, 512)
+        mask of shape: (batch_size, 1, 1, max_words)
+        """
+        # (batch_size, max_len, 512)
+        query = self.query(query)
+        key = self.key(key)        
+        value = self.value(value)   
+        
+        # (batch_size, max_len, 512) --> (batch_size, max_len, h, d_k) --> (batch_size, h, max_len, d_k)
+        query = query.view(query.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)    # swaps 1st and 2nd dim
+        key = key.view(key.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)  
+        value = value.view(value.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)
+
+        # (batch_size, h, max_len, d_k) matmul (batch_size, h, d_k, max_len) --> (batch_size, h, max_len, max_len)
+        scores = torch.matmul(query, key.permute(0,1,3,2)) / math.sqrt(query.size(-1)) #query.size(-1) = d_k
+    #we use mask to not compute calculation on padded elemnts
+        # (batch_size, h, max_len, d_k) matmul (batch_size, h, d_k, max_len) --> (batch_size, h, max_len, max_len)
+        
+        #mask fill replaces the values in original tensor with very small value, where the  position to replace is calculated by the 0 in mask matrix
+
+        scores = torch.matmul(query, key.permute(0,1,3,2)) / math.sqrt(query.size(-1))
+        scores = scores.masked_fill(mask == 0, -1e9)    # (batch_size, h, max_len, max_len)
+        weights = F.softmax(scores, dim = -1)           # (batch_size, h, max_len, max_len)
+        weights = self.dropout(weights)
+        # (batch_size, h, max_len, max_len) matmul (batch_size, h, max_len, d_k) --> (batch_size, h, max_len, d_k)
+        context = torch.matmul(weights, value)
+        # (batch_size, h, max_len, d_k) --> (batch_size, max_len, h, d_k) --> (batch_size, max_len, h * d_k)
+        context = context.permute(0,2,1,3).contiguous().view(context.shape[0], -1, self.heads * self.d_k)
+        # (batch_size, max_len, h * d_k)
+        interacted = self.concat(context)
+        return interacted
 
 # %%
