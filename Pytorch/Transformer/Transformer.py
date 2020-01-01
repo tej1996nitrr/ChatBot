0bb55b499d69cf74ebc95db69ab51fb18ebf356f
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-01-01 14:32:46 +0530

    created embeddings class , positional encoding

--- Pytorch/Transformer/Transformer.py
+++ Pytorch/Transformer/Transformer.py
@@ -173,8 +173,29 @@ def create_masks(question, reply_input, reply_target):
 
 
 # %%
-
-
+class Embeddings(nn.Module):
+    def __init__(self, vocab_size, d_model, max_len = 50):
+        super(Embeddings, self).__init__()
+        self.d_model = d_model
+        self.dropout = nn.Dropout(0.1)
+        self.embed = nn.Embedding(vocab_size, d_model)
+        self.pe = self.create_positinal_encoding(max_len, self.d_model)
+        self.dropout = nn.Dropout(0.1)
+        
+    def create_positinal_encoding(self, max_len, d_model):
+        pe = torch.zeros(max_len, d_model).to(device)
+        for pos in range(max_len):   # for each position of the word
+            for i in range(0, d_model, 2):   # for each dimension of the each position
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
+        pe = pe.unsqueeze(0)   # include the batch size (1,max_len,d_model) 1 will be expanded automatically
+        return pe
+        
+    def forward(self, encoded_words): #overriding the function from module
+        embedding = self.embed(encoded_words) * math.sqrt(self.d_model) #(batch_size,max_len,d_model)
+        embedding += self.pe[:, :embedding.size(1)]   # pe will automatically be expanded with the same batch size as encoded_words
+        embedding = self.dropout(embedding)
+        return embedding
 # %%
 
 # %%
