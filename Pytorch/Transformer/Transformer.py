9ca495f0ca87f6e34e01b46c19ed4e1fa3427cd2
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-01-04 14:51:11 +0530

    model definition and training function

--- Pytorch/Transformer/Transformer.py
+++ Pytorch/Transformer/Transformer.py
@@ -382,3 +382,61 @@ class LossWithLS(nn.Module):
         return loss
 
 # %%
+d_model = 512
+heads = 8
+num_layers = 3
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+epochs = 10
+
+with open('WORDMAP_corpus.json', 'r') as j:
+    word_map = json.load(j)
+    
+transformer = Transformer(d_model = d_model, heads = heads, num_layers = num_layers, word_map = word_map)
+transformer = transformer.to(device)
+adam_optimizer = torch.optim.Adam(transformer.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9)
+transformer_optimizer = AdamWarmup(model_size = d_model, warmup_steps = 4000, optimizer = adam_optimizer)
+criterion = LossWithLS(len(word_map), 0.1)
+
+# %%
+def train(train_loader, transformer, criterion, epoch):
+    
+    transformer.train() #dropout acts differently in training and testing so we initialize as train
+    sum_loss = 0
+    count = 0
+
+    for i, (question, reply) in enumerate(train_loader):
+        
+        samples = question.shape[0]
+
+        # Move to device
+        question = question.to(device)
+        reply = reply.to(device)
+
+        # Prepare Target Data
+        reply_input = reply[:, :-1] #last token is end token so se dont include that
+        reply_target = reply[:, 1:] #skip start token 
+
+        # Create mask and add dimensions
+        question_mask, reply_input_mask, reply_target_mask = create_masks(question, reply_input, reply_target)
+
+        # Get the transformer outputs
+        out = transformer(question, question_mask, reply_input, reply_input_mask)
+
+        # Compute the loss
+        loss = criterion(out, reply_target, reply_target_mask)
+        
+        # Backprop
+        transformer_optimizer.optimizer.zero_grad()
+        loss.backward()
+        transformer_optimizer.step()
+        
+        sum_loss += loss.item() * samples
+        count += samples
+        
+        if i % 100 == 0:
+            print("Epoch [{}][{}/{}]\tLoss: {:.3f}".format(epoch, i, len(train_loader), sum_loss/count))
+
+
+
+
+# %%
